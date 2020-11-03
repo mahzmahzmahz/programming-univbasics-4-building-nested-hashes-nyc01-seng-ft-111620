40957de9ee559ca2306bd5c2c5ca7bead56fcf9b
Alex Marz <alexhmarz@gmail.com>
2020-11-03 17:00:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,17 @@ monopoly = {
     :railroads=> {
       :pieces=>4,
       :rent_in_dollars=>{
-        :one_piece_owned=>
+        :one_piece_owned=>25,
+        :two_pieces_owned=>50,
+        :three_pieces_owned=>100,
+        :four_pieces_owned=>200
       },
-      :names=>{}
+      :names=>{
+        :reading_railroad=>{},
+        :pennsylvania_railroad=>{},
+        :b_and_o_railroad=>{},
+        :shortline_railroad=>{}
+      }
     }
   
   }
